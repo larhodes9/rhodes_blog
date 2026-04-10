14200a6855cdb865db0d6cd1556981bee830aba4
larhodes9 <lancerhodes@weber.edu>
2026-04-10 10:01:02 -0600

    Added allowed hosts

--- rhodes_site/settings.py
+++ rhodes_site/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-i66k$kq1zer5w9m^aya#4x*fu(^2@76!!_98=2x%xwg=zb-1+r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['rhodes-site.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
